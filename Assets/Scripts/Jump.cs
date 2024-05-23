6fc15deffa5f5321010e4276e575f3d65e8c0f17
unknown <antoniotisza@gmail.com>
2024-05-23 10:15:52 -0500

    Salto Completo

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody2D myRBD2D;
+    public float m_Thrust = 0.1f;
+    private bool isStarted = true;
+    private bool isJump = false;
+    void Start()
+    {
+        myRBD2D = GetComponent<Rigidbody2D>();
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started && isStarted && !isJump)
+        {
+            StartCoroutine(JumpCorroutine());
+        }
+        if (context.canceled && !isJump)
+        {
+            myRBD2D.AddForce(transform.up *  -m_Thrust * 0.2f, ForceMode2D.Impulse);
+        }
+    }
+    IEnumerator JumpCorroutine()
+    {
+        isJump = true;
+        myRBD2D.AddForce(transform.up * m_Thrust, ForceMode2D.Impulse);
+        isStarted = false;             
+        yield return new WaitForSeconds(1.5f);
+        isStarted = true;
+    }
+}
